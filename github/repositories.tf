0e9321a6add496b0ca95f47a110baee790d76cb1
Toby Scott <45221333+tobyscott25@users.noreply.github.com>
2022-11-18 20:49:25 +1100

    Create Beacon API

--- github/repositories.tf
+++ github/repositories.tf
@@ -5,21 +5,30 @@
 # }
 # Terraform can't manage repos that begin with a period.
 
+resource "github_repository" "beacon-api" {
+	name 					= "beacon-api"
+	description 			= "Safety Beacon API"
+	vulnerability_alerts 	= true
+	has_issues 				= true
+	has_projects 			= true
+	has_wiki 				= true
+}
+
 resource "github_repository" "infrastructure" {
-	name 			= "infrastructure"
-	description 	= "Infrastructure as Code"
-	has_downloads 	= true
-	has_issues 		= true
-	has_projects 	= true
-	has_wiki 		= true
+	name 					= "infrastructure"
+	description 			= "Infrastructure as Code"
+	vulnerability_alerts 	= true
+	has_issues 				= true
+	has_projects 			= true
+	has_wiki 				= true
 }
 
 resource "github_repository" "website" {
-	name 			= "website"
-	description 	= "Safety Beacon's website"
-	homepage_url 	= "https://safetybeacon.app"
-	has_downloads 	= true
-	has_issues 		= true
-	has_projects 	= true
-	has_wiki 		= true
+	name 					= "website"
+	description 			= "Safety Beacon's website"
+	homepage_url 			= "https://safetybeacon.app"
+	vulnerability_alerts 	= true
+	has_issues 				= true
+	has_projects 			= true
+	has_wiki 				= true
 }
